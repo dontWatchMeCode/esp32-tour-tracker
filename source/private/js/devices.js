c875ada3797e2f0bba5ba3c28958816be2fd5c16
OnlyPain <gschwandtner2k@protonmail.com>
2022-03-11 19:38:08 +0100

    different style for api keys

--- source/private/js/devices.js
+++ source/private/js/devices.js
@@ -17,7 +17,7 @@ if (window.location.href.indexOf("/devices") != -1) {
             .then(() => {
                 get_req.forEach((data, index) => {
                     output
-                        += "<tr> <th scope='col' class='d-none d-xl-table-cell text-center api-key'>" + data[1] + "</th> <th scope='col'> <div class='input-group align-items-center justify-content-center flex-nowrap'> <div class='form-outline float-start'> <input type='text' class='form-control input-name placeholder-active' value=" + data[0] + "> <div class='form-notch'> <div class='form-notch-leading' style='width: 9px;'></div> <div class='form-notch-middle' style='width: 0px;'></div> <div class='form-notch-trailing'></div> </div> </div> <button type='button' class='btn btn-outline-dark d-table-cell d-xl-none copy-key'> <i class='fas fa-copy'></i> </button> <button type='button' class='btn btn-outline-dark d-table-cell d-xl-none del-key'> <i class='fas fa-trash'></i> </button> </div> </th> <th class='d-none d-xl-table-cell'> <button type='button' class='btn btn-outline-dark copy-key'> <i class='fas fa-copy'></i> </button> <button type='button' class='btn btn-outline-dark del-key'> <i class='fas fa-trash'></i> </button> </th></tr>";
+                        += "<tr> <th scope='col' class='d-none d-xl-table-cell text-center api-key'> <div class='form-outline'> <input class='form-control active' type='text' value='" + data[1] + "' aria-label='readonly api key' readonly='' disabled> <div style='position:absolute; left:0; right:0; top:0; bottom:0;'></div> <label class='form-label' style='margin-left: 0px;'>Readonly</label> <div class='form-notch'> <div class='form-notch-leading' style='width: 9px;'></div> <div class='form-notch-middle' style='width: 60px;'></div> <div class='form-notch-trailing'></div> </div> </div> </th> <th scope='col'> <div class='input-group align-items-center justify-content-center flex-nowrap'> <div class='form-outline float-start'> <input type='text' class='form-control input-name placeholder-active' value=" + data[0] + "> <div class='form-notch'> <div class='form-notch-leading' style='width: 9px;'></div> <div class='form-notch-middle' style='width: 0px;'></div> <div class='form-notch-trailing'></div> </div> </div> <button type='button' class='btn btn-outline-dark d-table-cell d-xl-none copy-key'> <i class='fas fa-copy'></i> </button> <button type='button' class='btn btn-outline-dark d-table-cell d-xl-none del-key'> <i class='fas fa-trash'></i> </button> </div> </th> <th class='d-none d-xl-table-cell'> <button type='button' class='btn btn-outline-dark copy-key'> <i class='fas fa-copy'></i> </button> <button type='button' class='btn btn-outline-dark del-key'> <i class='fas fa-trash'></i> </button> </th></tr>";
                 });
                 container.innerHTML = output;
                 listeners();
@@ -36,9 +36,10 @@ if (window.location.href.indexOf("/devices") != -1) {
     }
 
     function listeners() {
-        document.querySelectorAll('.api-key').forEach((item, index) => {
+        document.querySelectorAll('.api-key > div').forEach((item, index) => {
+            api_key = document.querySelectorAll('.api-key input')[index_calc(index)].value.trim();
+            console.log(api_key);
             item.addEventListener('click', event => {
-                api_key = document.querySelectorAll('.api-key')[index_calc(index)].innerText.trim();
                 navigator.clipboard.writeText(api_key);
                 alert("API key kopiert:\n\n" + api_key.substr(0, 40) + "...");
             })
@@ -72,7 +73,7 @@ if (window.location.href.indexOf("/devices") != -1) {
 
         document.querySelectorAll('.copy-key').forEach((item, index) => {
             item.addEventListener('click', event => {
-                api_key = document.querySelectorAll('.api-key')[index_calc(index)].innerText.trim();
+                api_key = document.querySelectorAll('.api-key input')[index_calc(index)].value.trim();
                 navigator.clipboard.writeText(api_key);
                 alert("API key kopiert:\n\n" + api_key.substr(0, 40) + "...");
             })
