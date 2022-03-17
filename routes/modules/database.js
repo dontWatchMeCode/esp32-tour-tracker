d6836218fb87023d94c2e34f8a2b20680cbf7c50
Your Name <gschwandtner2k@protonmail.com>
2022-03-17 17:10:06 +0100

    removed try; some cleanup

--- routes/modules/database.js
+++ routes/modules/database.js
@@ -4,113 +4,94 @@ const prisma = new PrismaClient();
 const tools = require('./tools');
 
 async function check_user(arg) {
-    try {
-        await prisma.user.upsert({
-            where: { key: arg, },
-            update: {},
-            create: { key: arg, },
-        });
+    await prisma.user.upsert({
+        where: { key: arg, },
+        update: {},
+        create: { key: arg, },
+    });
 
-        const db_user = await prisma.user.findUnique({
-            where: {
-                key: arg
-            }
-        });
-
-        const fs = require('fs');
-        const dir = './uploads/' + db_user.id;
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
+    const db_user = await prisma.user.findUnique({
+        where: {
+            key: arg
         }
-    }
-    catch (error) {
-        return error;
+    });
+
+    const fs = require('fs');
+    const dir = './uploads/' + db_user.id;
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
     }
 }
 
 async function get_userid(arg) {
-    try {
-        const db_user = await prisma.user.findUnique({
-            where: { key: arg }
-        });
-        return db_user.id;
-    }
-    catch (error) {
-        return error;
-    }
+    const db_user = await prisma.user.findUnique({
+        where: { key: arg }
+    });
+    return db_user.id;
 }
 
 async function api_keys_get(arg) {
-    try {
-        const db_user = await prisma.user.findUnique({
-            where: { key: arg, },
-            include: {
-                api_keys: {
-                    select: { key: true, name: true }
-                }
-            },
-        });
+    const db_user = await prisma.user.findUnique({
+        where: { key: arg, },
+        include: {
+            api_keys: {
+                select: { key: true, name: true }
+            }
+        },
+    });
 
-        return db_user.api_keys;
-    }
-    catch (error) {
-        return error;
-    }
+    return db_user.api_keys;
 }
 
 async function api_keys_add(arg) {
     const userid = await get_userid(arg);
-    try {
-        await prisma.api_keys.create({
-            data: {
-                userId: userid,
-                key: tools.makekey(64),
-                name: "Tracker"
-            }
-        });
-    }
-    catch (error) { console.log(error); }
+    await prisma.api_keys.create({
+        data: {
+            userId: userid,
+            key: tools.makekey(64),
+            name: "Tracker"
+        }
+    });
 }
 
 async function api_keys_delete(arg) {
-    try {
-        await prisma.api_keys.delete({
-            where: { key: arg }
-        });
-    }
-    catch (error) { console.log(error); }
+    await prisma.api_keys.delete({
+        where: { key: arg }
+    });
 }
 
 async function api_keys_update(arg, value) {
-    try {
-        await prisma.api_keys.update({
-            where: { key: arg },
-            data: { name: value },
-        });
-    }
-    catch (error) { console.log(error); }
+    await prisma.api_keys.update({
+        where: { key: arg },
+        data: { name: value },
+    });
 }
 
 async function files_get(arg) {
-    let out_tours;
+    let db_user;
     const userid = await get_userid(arg);
+
     let file_list = require('fs')
         .readdirSync('./uploads/' + userid);
 
-    const db_count = await prisma.user.findUnique({
-        where: { key: arg, },
-        include: {
-            tours: {
-                select: { file: true }
+    async function get() {
+        db_user = await prisma.user.findUnique({
+            where: { key: arg, },
+            include: {
+                tours: {
+                    select: { name: true, file: true }
+                }
             }
-        }
-    });
-    if (db_count.tours.length != file_list.length) {
+        });
+    }
+    await get();
+
+    if (db_user.tours.length != file_list.length) {
         // delete all records where file doasnt exist
-        for (let i = 0; i < db_count.tours.length; i++) {
-            if (!file_list.includes(db_count.tours[i].file)) {
+        for (let i = 0; i < db_user.tours.length; i++) {
+            if (!file_list.includes(db_user.tours[i].file)) {
                 await prisma.tours.delete({
-                    where: { file: db_count.tours[i].file }
+                    where: { file: db_user.tours[i].file }
                 });
             }
         }
@@ -122,15 +103,8 @@ async function files_get(arg) {
                 create: { file: file_list[i], userId: userid },
             });
         }
+        await get();
     }
-    const db_user = await prisma.user.findUnique({
-        where: { key: arg, },
-        include: {
-            tours: {
-                select: { name: true, file: true }
-            }
-        },
-    });
 
     return db_user.tours;
 }
