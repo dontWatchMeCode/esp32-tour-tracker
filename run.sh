3ef233dc42c7f3397b834cd4852dfe596ff80ef3
OnlyPain <gschwandtner2k@protonmail.com>
2022-03-26 17:31:11 +0100

    added check for git

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/sh
+
+# checks git (https://stackoverflow.com/a/3278427)
 # build / recreats the containers
 # if db exist it will not be deleted
 
-docker-compose up --build -d --no-cache
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    docker-compose up --build -d --no-cache
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
