9eb1000498eac53423bf36a79938f473136487cd
OnlyPain <gschwandtner2k@protonmail.com>
2022-03-27 19:16:03 +0200

    added port mapping option in dev (mysql)

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@
 # if db exist it will not be deleted
 
 # -f for force update
+# -d adds port mapping for mysql
 
 UPSTREAM=${1:-'@{u}'}
 BRANCH="main" # invalid branch will trow error
@@ -15,17 +16,29 @@ BASE=$(git merge-base @ "${BRANCH}")
 
 COUNT=$(docker-compose ps | wc -l)
 
-if [ "$1" = "-f" ]; then
-    echo "Force update"
-    echo "------------"
-    docker-compose up --build -d
-    exit
+if [ $# -gt 0 ]; then
+    if [[ $@ = *"-d"* ]]; then
+        echo "Added port mapping (MySQL)"
+        echo "    # temp dev ports" >>docker-compose.yml
+        echo "    ports:" >>docker-compose.yml
+        echo "      - '127.0.0.1:3306:3306'" >>docker-compose.yml
+    fi
+    if [[ $@ = *"-f"* ]]; then
+        echo "Force update"
+        echo "------------"
+        docker-compose up --build -d
+        if [[ $@ = *"-d"* ]]; then
+            git checkout docker-compose.yml >/dev/null 2>&1
+        fi
+        exit
+    fi
 fi
 
 if [ $COUNT -lt 4 ]; then
     echo "No Container"
     echo "------------"
     docker-compose up --build -d
+    exit
 fi
 
 if [ $LOCAL = $REMOTE ]; then
@@ -42,4 +55,8 @@ elif [ $REMOTE = $BASE ]; then
 else
     echo "Diverged"
     echo "--------"
+fi
+
+if [[ $@ = *"-d"* ]]; then
+    git checkout docker-compose.yml >/dev/null 2>&1
 fi
