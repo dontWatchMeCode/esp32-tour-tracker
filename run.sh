a61bd1ec37e121821b2d4bb367142103fcff5127
OnlyPain <gschwandtner2k@protonmail.com>
2022-03-27 18:20:14 +0200

    added force update option, cleanup

--- run.sh
+++ run.sh
@@ -1,29 +1,45 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 # checks git (https://stackoverflow.com/a/3278427)
 # build / recreats the containers
 # if db exist it will not be deleted
 
+# -f for force update
+
 UPSTREAM=${1:-'@{u}'}
+BRANCH="main" # invalid branch will trow error
+
 LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+REMOTE=$(git rev-parse "${BRANCH}")
+BASE=$(git merge-base @ "${BRANCH}")
 
 COUNT=$(docker-compose ps | wc -l)
 
+if [ "$1" = "-f" ]; then
+    echo "Force update"
+    echo "------------"
+    docker-compose up --build -d
+    exit
+fi
+
 if [ $COUNT -lt 4 ]; then
     echo "No Container"
+    echo "------------"
     docker-compose up --build -d
 fi
 
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
+    echo "----------"
 elif [ $LOCAL = $BASE ]; then
     echo "Need to pull"
+    echo "------------"
     git pull
     docker-compose up --build -d
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
+    echo "------------"
 else
     echo "Diverged"
+    echo "--------"
 fi
