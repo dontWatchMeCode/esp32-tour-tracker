dc9c42c41fb2cc87f9a0eea58c50ee58ea4e9725
OnlyPain <gschwandtner2k@protonmail.com>
2022-04-14 21:48:54 +0200

    added new version of run script

--- run.sh
+++ run.sh
@@ -1,62 +1,63 @@
 #!/usr/bin/bash
 
-# checks git (https://stackoverflow.com/a/3278427)
-# build / recreats the containers
-# if db exist it will not be deleted
-
-# -f for force update
-# -d adds port mapping for mysql
-# -s stops node container after creation
-
-if [[ $@ = *"-h"* ]]; then
-    echo "-f for force update"
-    echo "-d adds port mapping for mysql"
-    echo "-s stops node container"
-    exit
-fi
-
-GITUPLL=$(git pull)
-
-COUNT=$(docker-compose ps | wc -l)
-
 if [ $# -gt 0 ]; then
-    if [[ $@ = *"-d"* ]]; then
-        echo "Added port mapping (MySQL)"
-        echo "    # temp dev ports" >>docker-compose.yml
-        echo "    ports:" >>docker-compose.yml
-        echo "      - '127.0.0.1:3306:3306'" >>docker-compose.yml
+    if [[ $@ = *"-dev"* ]]; then
+        sed -i -e 's/@database/@localhost/g' .env
+
+        echo "    ports: # temp dev ports" >>docker-compose.yml
+        echo "      - '127.0.0.1:3306:3306' # temp dev ports" >>docker-compose.yml
+
+        docker-compose up -d db
+        docker-compose stop node
+        npm run db
+
+        sed -i -e "s/    ports: # temp dev ports//g" docker-compose.yml
+        sed -i -e "s/      - '127.0.0.1:3306:3306' # temp dev ports//g" docker-compose.yml
+        sed -i -Ez '$ s/\n+$//' docker-compose.yml
+        echo "" >>docker-compose.yml
+
+        npm run dev
+        exit 1
     fi
+
+    if [[ $@ = *"-l"* ]]; then
+        docker-compose logs -f node
+        exit 1
+    fi
+
+    if [[ $@ = *"-g"* ]]; then
+        if [ "$(git pull)" = "Already up to date." ]; then
+            echo "Up-to-date"
+            echo "----------"
+        else
+            echo "Need to Update"
+            echo "--------------"
+
+            sed -i -e 's/@localhost/@database/g' .env
+            docker-compose up --build -d
+        fi
+    fi
+
     if [[ $@ = *"-f"* ]]; then
         echo "Force update"
         echo "------------"
+
+        sed -i -e 's/@localhost/@database/g' .env
         docker-compose up --build -d
-        if [[ $@ = *"-d"* ]]; then
-            git checkout docker-compose.yml >/dev/null 2>&1
-        fi
-        exit
+
+        read -p "View log of node container (y/n)?" yn
+        case $yn in
+        [Yy]*)
+            docker-compose logs -f node
+            exit 1
+            ;;
+        *) exit 1 ;;
+        esac
     fi
-fi
-
-if [ "$COUNT" -lt 4 ]; then
-    echo "Container missing"
-    echo "-----------------"
-    docker-compose up --build -d
-    exit
-fi
-
-if [ "$GITUPLL" = "Already up to date." ]; then
-    echo "Up-to-date"
-    echo "----------"
 else
-    echo "Need to Update"
-    echo "--------------"
-    docker-compose up --build -d
-fi
-
-if [[ "$@" = *"-d"* ]]; then
-    git checkout docker-compose.yml >/dev/null 2>&1
-fi
-
-if [[ "$@" = *"-s"* ]]; then
-    docker-compose stop node
+    echo "-dev  build db container; npm run dev"
+    echo "-f    force recreate docker containers"
+    echo "-g    git pull; build containers if needed"
+    echo "-l    view logs of node container"
+    exit
 fi
